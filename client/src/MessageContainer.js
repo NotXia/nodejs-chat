5cca7f94370755e7ba45a2b8d8e7c9b24f341f0d
Xia <tiancheng.xia2001@gmail.com>
2021-02-27 09:26:15 +0100

    Auto scroll to bottom mechanism

--- client/src/MessageContainer.js
+++ client/src/MessageContainer.js
@@ -7,28 +7,51 @@ class MessageContainer extends React.Component {
         super(props);
         this.state = {};
     }
+
+    scrollToBottom() {
+        this.messagesEnd.scrollIntoView({ behavior: "auto" });
+    }
     
+    componentDidMount() {
+        this.needScrollToBottom = true;
+
+        this.chatBox.addEventListener('scroll', () => {
+            /* Checks if the scroll bar is at the bottom */
+            this.needScrollToBottom = this.chatBox.scrollHeight - Math.ceil(this.chatBox.scrollTop) <= this.chatBox.clientHeight;
+        });
+    }
+
+    componentDidUpdate() {
+        /* Scrolls the chat to the bottom, if needed */
+        if (this.needScrollToBottom) {
+            this.scrollToBottom();
+        }
+    }
+
     render() {
         const messages = this.props.messages;
-        const messagesComponents = messages.map((message) => {
-            if (message.type == "join") {
-                return <UserJoinMessage user={message.user} />
-            }
-            else if (message.type == "quit") {
-                return <UserQuitMessage user={message.user} />
-            }
-            else {
-                if (message.user === this.props.curr_user) {
-                    return <OwnMessage message={message.message} key={message.id} /> 
-                }
-                else {
-                    return <OtherMessage user={message.user} message={message.message} key={message.id} />
-                }
+        const messagesComponents = messages.map((message, i) => {
+            switch (message.type) {
+                case "join":
+                    return <UserJoinMessage user={message.user} key={i} />
+
+                case "quit":
+                    return <UserQuitMessage user={message.user} key={i} />
+
+                default:
+                    if (message.user === this.props.curr_user) {
+                        return <OwnMessage message={message.message} key={i} /> 
+                    }
+                    else {
+                        return <OtherMessage user={message.user} message={message.message} key={i} />
+                    }
             }
         });
+
         return (
-            <div className="chat-box">
+            <div className="chat-box" ref={(el) => { this.chatBox = el; }} >
                 {messagesComponents}
+                <div style={{clear: "both"}} ref={(el) => { this.messagesEnd = el; }} /> 
             </div>
         );
     }
